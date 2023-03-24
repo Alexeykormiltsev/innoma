f4a3a74900e6c25e9b78ac906ad331e9fd28feaf
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-03-24 17:55:55 +0500

    немного не справился знаний не достаточно

--- Template/js/main.js
+++ Template/js/main.js
@@ -80,6 +80,12 @@ $(function () {
         $('.burger').toggleClass('burger--active');
         $('body').toggleClass('hidden');
     });
+    $('.myBtn').on('click', function () {
+        $('body').toggleClass('hidden');
+    });
+    $('.close,.modal').on('click', function () {
+        $('body').removeClass('hidden');
+    });
 
 
 
@@ -111,4 +117,35 @@ $(function () {
         },
     });
 });
+var modal = document.getElementById("myModal");
 
+var btn = document.getElementsByClassName("myBtn");
+
+var span = document.getElementsByClassName("close");
+
+function buttonClick(event) {
+    var id = this.getAttribute('data-modal');
+    var modal = document.getElementById(id);
+    modal.style.display = 'block';
+}
+
+for (var i = 0; i < btn.length; i++) {
+    btn[i].onclick = buttonClick;
+}
+
+function spanClick() {
+    var id = this.getAttribute('data-modal');
+    var modal = document.getElementById(id);
+    modal.style.display = 'none';
+}
+
+for (var i = 0; i < span.length; i++) {
+    span[i].onclick = spanClick;
+}
+
+window.onclick = function (event) {
+    var isModal = (' ' + event.target.className + ' ').indexOf(' modal ') > -1;
+    if (isModal) {
+        event.target.style.display = "none";
+    }
+}
